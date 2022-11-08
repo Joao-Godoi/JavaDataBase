39a21e0065ce1e8892fea4a663bf7e06c686f6e4
João Godoi <joaogodoi.dev@gmail.com>
2022-11-08 20:33:55 -0300

    :sparkles: Feat: Created department class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,59 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Department)) {
+            return false;
+        }
+        Department department = (Department) o;
+        return Objects.equals(id, department.id) && Objects.equals(name, department.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            "}";
+    }
+
+}
